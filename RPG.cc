3e5a5fc1749fb7c41d648009c275f953a2c4afbc
DvnMauricio <00050726@uca.edu.sv>
2026-09-08 12:27:33 -0600

    feat: add funcion RecibirDano

--- RPG.cc
+++ RPG.cc
@@ -1,1 +1,38 @@
 #include <iostream>
+
+struct personaje
+{
+    int vida;
+};
+
+void ConsultarVida(int vida);
+void RecibirDano(int* vida, int dano);
+
+int main ()
+{
+    personaje Personaje;
+
+    Personaje.vida = 100;
+
+    std::cout << "Vida inicial:" << std::endl;
+    ConsultarVida(Personaje.vida);
+
+    RecibirDano(&Personaje.vida, 30);
+
+    std::cout << "Vida despues de recibir dano:" << std::endl;
+    ConsultarVida(Personaje.vida);
+
+
+
+
+    }
+
+void ConsultarVida(int vida)
+{
+    std::cout << "Vida actual: " << vida << std::endl;
+}
+
+void RecibirDano(int* vida, int dano)
+{
+     *vida -= dano;
+}
