d32297960fff51a6932389b6ee0544de9dab715c
AdolfoDiaz07 <00134526@uca.edu.sv>
2026-09-08 12:20:55 -0600

    Adicion de funciones

--- RPG.cc
+++ RPG.cc
@@ -1,1 +1,42 @@
 #include <iostream>
+
+struct personaje
+{
+    int vida;
+};
+
+void ConsultarVida(int vida);
+void Curar(int &vida, int puntos);
+int main()
+{
+    personaje Personaje;
+
+    Personaje.vida = 90;
+
+    std::cout << "Vida inicial: " << std::endl;
+    ConsultarVida(Personaje.vida);
+
+    Curar(Personaje.vida, 10);
+
+    std::cout << "Despues de curar: " << std::endl;
+    ConsultarVida(Personaje.vida);
+
+    return 0;
+}
+
+
+void ConsultarVida(int vida)
+{
+    std:: cout<<"Vida actual: "<< vida << "hp" << std::endl;
+}
+
+void Curar(int &vida, int puntos)
+{
+    if (vida<100){
+    vida += puntos;
+    std:: cout<<"La vida del personaje a sido aumentada"<< std::endl;
+    }
+    else{
+        std::cout<<"Error, vida maxima alcanzada"<<std::endl;
+    }
+}
